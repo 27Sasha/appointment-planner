dd662350803e4f1733264809d51e3528b0650cbd
27Sasha <134843865+27Sasha@users.noreply.github.com>
2024-12-31 14:10:02 +1100

    Add files via upload

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,50 +1,49 @@
 import React, { useState } from "react";
+
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
+import { Tile } from "../../components/tile/Tile";
 
-// Extract appointments and addAppointment props
-export const AppointmentsPage = ({ appointments, addAppointment, contacts }) => {
-  //Define state variables
-  const [title, setTitle] = useState("");
-  const [contact, setContact] = useState(contacts.length > 0 ? contacts[0].name : "");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
+export const AppointmentsPage = ({contactList, appointmentList, addApp}) => {
+  /*
+  Define state variables for 
+  appointment info
+  */
+  const [inputName, setInputName] = useState('')
+  const [inputContact, setInputContact] = useState('')
+  const [inputDate, setInputDate] = useState('')
+  const [inputTime, setInputTime] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add appointment info and clear data  
-    addAppointment(title, contact, date, time);
-    setTitle("");
-    setContact("");
-    setDate("");
-    setTime("");
+    /*
+    Add contact info and clear data  
+    */
+   addApp({
+    name: inputName,
+    contact: inputContact,
+    date: inputDate,
+    time: inputTime
+   })
+
+    setInputName('')
+    setInputContact('')
+    setInputDate('')
+    setInputTime('')
+
   };
 
   return (
     <div>
       <section>
-        <h2>Add Appointment</h2>
-        {/* Render AppointmentForm with props */}
-        <AppointmentForm
-          contacts={contacts}
-          title={title}
-          setTitle={setTitle}
-          contact={contact}
-          setContact={setContact}
-          date={date}
-          setDate={setDate}
-          time={time}
-          setTime={setTime}
-          handleSubmit={handleSubmit}
-        />
+          <h2>Add Appointment</h2>
+          <AppointmentForm title = {inputName} contacts = {inputContact} date = {inputDate} time = {inputTime} setTitle = {setInputName} setContact = {setInputContact} settime = {setInputTime} setDate = {setInputDate} handleSubmit = {handleSubmit}/>
+
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
-        {/* Render TileList with props */}
-        <TileList
-          tiles={appointments}
-        />
+        <TileList appointmentList= {appointmentList} />
       </section>
     </div>
   );

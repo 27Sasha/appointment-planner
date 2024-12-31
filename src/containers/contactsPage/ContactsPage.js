ed0366096ce95454a93d37a3c98f1f4439dfe055
27Sasha <134843865+27Sasha@users.noreply.github.com>
2024-12-31 14:10:23 +1100

    Add files via upload

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,67 +1,54 @@
 import React, { useState, useEffect } from "react";
+
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-// Extract contacts and addContact props
-export const ContactsPage = ({ contacts, addContact }) => {
-  // Define state variables
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [duplicate, setDuplicate] = useState(false);
+export const ContactsPage = ({contacts, addContact}) => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+ const [currentName, setCurrentName] = useState('')
+ const [currentPhone, setCurrentPhone] = useState('')
+ const [currentEmail, setCurrentEmail] = useState('')
+ const [isDup, setisDup] = useState(false)
 
+ useEffect(() =>{
+  setisDup(contacts.some(contact =>
+    contact.name === currentName
+  ))
+ }, [props.contactList.name, currentName.name])
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add contact info and clear data if contact name is not a duplicate
-    if (!duplicate) {
-      addContact(name, phone, email);
-      setName("");
-      setPhone("");
-      setEmail("");
-    }
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+   
+   if (!isDup){
+    addContact(currentName, currentPhone, currentEmail)
+    setCurrentName('')
+    setCurrentPhone('')
+    setCurrentEmail('')
+
+   }
   };
 
-  // useEffect hook that checks if the contact name is a duplicate 
-  useEffect(() => {
-    const nameIsDuplicate = () => {
-      const nameFound = contacts.find((contact) => contact.name === name);
-      if (nameFound !== undefined) {
-        return true;
-      }
-      return false;
-    };
-
-    if (nameIsDuplicate()) {
-      setDuplicate(true);
-    } else {
-      setDuplicate(false);
-    }
-  }, [name, contacts, duplicate])
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
 
   return (
     <div>
       <section>
-        {/* Notify user if contact name is a duplicate */}
-        <h2>Add Contact {duplicate ? " - This contact name already exists" : ""}</h2>
-
-        {/* Render ContactForm with props */}
-        <ContactForm 
-          name={name}
-          setName={setName}
-          phone={phone}
-          setPhone={setPhone}
-          email={email}
-          setEmail={setEmail}
-          handleSubmit={handleSubmit}
-        />
+        <h2>Add Contact</h2> 
+        <ContactForm name= {currentName} email = {currentEmail} phone = {currentPhone} setName = {setCurrentName} setEmail={setCurrentEmail} setPhone={setCurrentPhone} handleSubmit = {handleSubmit} />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
-        {/* Render TileList with props */}
-        <TileList 
-          tiles={contacts} 
-        />
+        <TileList contactList = {contacts}/>
       </section>
     </div>
   );

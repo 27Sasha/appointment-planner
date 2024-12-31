c522220ead68a805d37686d00d0cff939486137e
27Sasha <134843865+27Sasha@users.noreply.github.com>
2024-12-31 14:12:44 +1100

    Add files via upload

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,22 +1,16 @@
 import React from "react";
 
-// Extract contacts array, contact names, and onChange callback events
-export const ContactPicker = ({ name, onChange, contacts }) => {
+export const ContactPicker = ({ contactList, value, name, onChange }) => {
   return (
-    // Render select element with the onChange attribute set to the callback function 
-    <select name={name} onChange={onChange}>
-      {/* Default option element */}
-      <option value={""} key={-1} selected="selected">
-        No Contact Selected
-      </option>
-      {/* Iteratively add option elements using contact names from contacts array */}
-      {contacts.map((contact) => {
-        return (
-          <option value={contact} key={contact}>
-            {contact}
-          </option>
-        );
+    <>
+    <select onChange = {onChange} value={value} name = {name}>
+      <option value={''}>No Contact Selected</option>
+      {contactList.map((contact) => {
+        return(
+          <option key={contact} value = {contact}>{contact}</option>
+        )
       })}
     </select>
+    </>
   );
 };
